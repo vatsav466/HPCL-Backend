9bea5da57dfe5628d76c0966203cc43e3c8bb18c
Keerthesre1 <139453571+Keerthesre1@users.noreply.github.com>
2024-10-28 15:40:04 +0530

    Added comments to the action files

--- ingest_api_manager/cris_actions.py
+++ ingest_api_manager/cris_actions.py
@@ -8,4 +8,14 @@ router = fastapi.APIRouter(prefix='/cris')
 # Action ingest_data
 @router.post('/ingest_data', tags=['CRIS'])
 async def cris_ingest_data(data: Cris_Ingest_DataParams):
+    """
+    Endpoint to ingest CRIS data.
+
+    Args:
+        data (Cris_Ingest_DataParams): The CRIS data parameters including vendor, 
+        authentication token, vendor ID, location ID, and optional data list.
+
+    Returns:
+        Response object indicating success or failure of the data ingestion process.
+    """
     ...
--- ingest_api_manager/va_actions.py
+++ ingest_api_manager/va_actions.py
@@ -8,4 +8,15 @@ router = fastapi.APIRouter(prefix='/va')
 # Action ingest_data
 @router.post('/ingest_data', tags=['VA'])
 async def va_ingest_data(data: Va_Ingest_DataParams):
+    """
+    Endpoint to ingest VA data.
+
+    Args:
+        data (Va_Ingest_DataParams): The VA data parameters including vendor, 
+        authentication token, vendor ID, location ID, location type, and 
+        optional data list.
+
+    Returns:
+        Response object indicating success or failure of the data ingestion process.
+    """
     ...
