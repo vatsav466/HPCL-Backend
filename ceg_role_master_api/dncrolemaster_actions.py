a634553365ed691e607cfba2e6c0b07804603113
Keerthesre1 <139453571+Keerthesre1@users.noreply.github.com>
2024-11-06 23:07:52 +0530

    Removed the upload file logic from the ceg and dnc role masters

--- ceg_role_master_api/dncrolemaster_actions.py
+++ ceg_role_master_api/dncrolemaster_actions.py
@@ -40,33 +40,5 @@ async def dncrolemaster_upload_dnc_role_master(uploadfile: fastapi.UploadFile = 
         HTTPException: If there is an error uploading the file (500).
         HTTPException: If there is an error processing the CSV file (400).
     """
-    redis_client = await urdhva_base.redispool.get_redis_connection()
-    upload_dir = urdhva_base.settings.mft_path
-    os.makedirs(upload_dir, exist_ok=True)
-    
-    # Define the file path
-    file_path = os.path.join(upload_dir, uploadfile.filename)
-    
-    # Save the uploaded file
-    try:
-        with open(file_path, "wb") as buffer:
-            shutil.copyfileobj(uploadfile.file, buffer)
-        data = pl.read_csv(file_path)
-        # Iterate through the rows of the CSV and extract `bu` and `sapid`
-        for row in data.iter_rows(named=True):
-            bu = row["bu"]  # Assuming 'bu' column exists in the CSV
-            sapid = row["sapid"]  # Assuming 'sapid' column exists in the CSV
-            
-            # Create a Redis key using the format bu_sapid
-            redis_key = f"{bu.upper()}_{sapid}"
-            
-            # Set the key in Redis with the value being the data row
-            await redis_client.hset("dnc_master", redis_key, json.dumps(row)) 
-            
-        # Display data or perform further processing as needed
-        return {"filename": file_path, "data": data.to_dicts()}  # Returns the first few rows as a sample
-    except Exception as e:
-        raise fastapi.HTTPException(status_code=500, detail="File upload failed.") from e
-    except Exception as e:
-        raise fastapi.HTTPException(status_code=400, detail="Failed to process CSV file.") from e
+    ...
 
