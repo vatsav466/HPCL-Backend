b70e05260f09b1d9d4a2751e7ad9da99c9a430d0
venugopalnaidu <venu@urdvapay.com>
2024-11-08 21:06:10 +0530

    updated installation script

--- code_sync.sh
+++ code_sync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Code Sync Started"
-
+InstallDir=`pwd`
 CodeDir='/opt/ceg/algo'
-for folder in UrdhvaBase api_manager ceg_role_master_api orchestrator utilities vendor_ingestion_api workflow
+for folder in UrdhvaBase api_manager ceg_role_master_api orchestrator utilities vendor_ingestion_api
 do
   if [ ! -d $CodeDir/$folder ]; then
     mkdir -p $CodeDir/$folder
@@ -16,19 +16,23 @@ rsync -azSP ceg_role_master_api/  $CodeDir/ceg_role_master_api/  --exclude .alg_
 rsync -azSP orchestrator/  $CodeDir/orchestrator/  --exclude .alg_env
 rsync -azSP utilities/  $CodeDir/utilities/  --exclude .alg_env
 rsync -azSP vendor_ingestion_api/  $CodeDir/vendor_ingestion_api/  --exclude .alg_env
-rsync -azSP workflow/  $CodeDir/workflow/  --exclude .alg_env
+
+# Copying all service files
+rsync -aS "$InstallDir"/services/base_configuration/system_services/* /etc/systemd/system/
+systemctl daemon-reload
+
+cd $CodeDir/UrdhvaBase
 
 # shellcheck disable=SC2006
 proxy_settings=`env | grep http_proxy`
-if [ -z "$proxy_settings" ]; then
-  pip_proxy=" --proxy $proxy_settings"
-else
-  pip_proxy=""
-fi
 
-cd $CodeDir/UrdhvaBase
-/opt/ceg/venv/bin/python -m pip install "$pip_proxy" -e .
-/opt/ceg/venv/bin/python -m pip install "$pip_proxy" -r requirements.txt
+if [ -z "$proxy_settings" ]; then
+    /opt/ceg/venv/bin/python -m pip install -e .
+    /opt/ceg/venv/bin/python -m pip install  -r "$InstallDir"/requirements.txt
+else
+  /opt/ceg/venv/bin/python -m pip install --proxy "$proxy_settings" -e .
+  /opt/ceg/venv/bin/python -m pip install --proxy "$proxy_settings" -r "$InstallDir"/requirements.txt
+fi
 
 mkdir -p /var/log/ceg_sys_logs/ | true
 mkdir -p /var/log/ceg_logs/ | true
