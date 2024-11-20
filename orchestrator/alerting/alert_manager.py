14125c5aad31c011d364480358d7a3fab1ccfbaa
venugopalnaidu <venu@urdvapay.com>
2024-11-21 03:30:46 +0530

    Updated new alerting functions schema

--- orchestrator/alerting/alert_manager.py
+++ orchestrator/alerting/alert_manager.py
@@ -54,3 +54,66 @@ async def close_alert(alert_data):
     elif alert_data['alert_type'] == 'EMLock':
         return await emlock_alert.EMLockAlertManager().close_bu_alert(alert_data)
     return None
+
+
+class AlertAction:
+    @classmethod
+    async def update_alert_data(cls, alert_data):
+        """
+        Function to update alert data, Either reject or approve or justify or override
+        :param alert_data:
+        :return:
+        """
+        function_map = {"Justification": "justify_alert", "Rejected": "reject_alert",
+                        "Approved": "approve_alert", "Override": "override_alert"}
+
+        # get the function name
+        function_name = function_map.get(alert_data['alert_action'], None)
+        if function_name:
+            # call the function
+            return await getattr(cls, function_name)(alert_data)
+        return None
+
+    @classmethod
+    async def base_functionality(cls, alert_data):
+        """
+        Function to update alert data, Either reject or approve or justify or override
+        :param alert_data:
+        :return:
+        """
+        # Todo:- here we have to write all the generic functionality like updating the alert data,
+        #  history, fetching users, roles, ...
+
+    @classmethod
+    async def reject_alert(cls, alert_data):
+        """
+        Function to reject an alert
+        :param alert_data:
+        :return:
+        """
+
+    @classmethod
+    async def approve_alert(cls, alert_data):
+        """
+        Function to approve an alert
+        :param alert_data:
+        :return:
+        """
+
+    @classmethod
+    async def justify_alert(cls, alert_data):
+        """
+        Function to justify an alert
+        :param alert_data:
+        :return:
+        """
+
+    @classmethod
+    async def override_alert(cls, alert_data):
+        """
+        Function to override an alert
+        :param alert_data:
+        :return:
+        """
+
+
