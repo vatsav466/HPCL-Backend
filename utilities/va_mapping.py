9130613704dc7ebd51afa8bec4183b7a39f593a8
YesurajuParusu468 <yesu.p@algofusiontech.com>
2024-12-04 19:49:17 +0530

    va_mapping added

---
+++ utilities/va_mapping.py
@@ -0,0 +1,18 @@
+va_interlock_mapping={
+    "OVERSTAYING_OF_TWO_WHEELERS": {
+        "interlock_name": "Safety Violations during decantation(VA)",
+        "description": "safety violation"
+    },
+    "OVERSTAYING_OF_TWO_WHEELERS": {
+        "interlock_name": "Vehicle count / Queue management(VA)",
+        "description": "vehicle controlling"
+    },
+    "PEOPLE_INSIDE_VEHICLE": {
+        "interlock_name": "Safety Violations during decantation(VA)",
+        "description": "safety violation"
+    },
+    "ABSENCE_OF_SAND_BUCKET_DECANTATION": {
+        "interlock_name": "Safety Violations during decantation(VA)",
+        "description": "safety violation"
+    }
+}
