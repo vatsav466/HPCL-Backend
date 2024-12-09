bfaf39681090c08f57abeaf6b6d7ae8f72c26250
amplifinn-admin <santosh@urdhvapay.com>
2024-12-09 17:56:00 +0530

    Bug Fix

--- api_manager/indentdryout_actions.py
+++ api_manager/indentdryout_actions.py
@@ -33,12 +33,23 @@ async def indentdryout_sync_data_from_cris_to_ceg(data: Indentdryout_Sync_Data_F
 # Action create_dry_out_alert
 @router.post('/create_dry_out_alert', tags=['IndentDryOut'])
 async def indentdryout_create_dry_out_alert(data: Indentdryout_Create_Dry_Out_AlertParams):
-    Charts_Connection_Vault_RoutingParams.connection_id = connection_mapping.connection_mapping.get("cris", "1")
+    Charts_Connection_Vault_RoutingParams.connection_id = connection_mapping.connection_mapping.get("hpcl_ceg", "1")
     Charts_Connection_Vault_RoutingParams.action = 'get_data'
     function = await charts_connection_vault_routing(Charts_Connection_Vault_RoutingParams)
     schema = connection_mapping.schema_mapping.get("cris", "public")
     table = connection_mapping.table_mapping.get("dry_out", "")
     query = f'''SELECT * FROM "{schema}"."{table}" WHERE "volume" > 0 AND "indent_status" != 'Raised' AND "status" IN ('0', '1', '2');'''
+    query = f'''select site_id, fcc_code, item_name,count(distinct tank_no) tank_cnt,
+            rosapcode, STRING_AGG(CAST(tank_no AS TEXT), ',') tank_no,
+            case when sum(pumpable_Stock) <=0 then 1
+            when sum(pumpable_Stock) <(sum(sch.avgsales_7days)/7) then 2
+            when sum(pumpable_Stock) between (sum(sch.avgsales_7days)/7) and (sum(sch.avgsales_7days)/7)*3 then 3
+            when sum(pumpable_Stock) between (sum(sch.avgsales_7days)/7)*3 and (sum(sch.avgsales_7days)/7)*6 then 4
+            else 6 end status
+            from "{schema}".{table} sch
+            where 1=1 and sch.volume>0 and sch.rosapcode = '41052982'
+            group by site_id, fcc_code, item_name, rosapcode
+            order by site_id, fcc_code, item_name, rosapcode'''
     records = await function(schema_name=schema, table_name=table, query=query)
     records = records.head(10).to_dicts()
 
@@ -54,7 +65,7 @@ async def indentdryout_create_dry_out_alert(data: Indentdryout_Create_Dry_Out_Al
         'severity': ""
     }
 
-    _mapping = await indent_dry_out({}).prod_code_mapping()
+    _mapping = await indent_dry_out().prod_code_mapping()
 
     for _dry in records:
         _dry['indent_status'] = 'Raised'
@@ -62,7 +73,7 @@ async def indentdryout_create_dry_out_alert(data: Indentdryout_Create_Dry_Out_Al
         # alert_data['product_code'] = _dry['product_no']
         alert_data['product_code'] = _mapping.get(_dry['item_name'], _dry['product_no'])
         alert_data['sap_id'] = _dry['site_id']
-        alert_data['device_id'] = _dry['tank_no']
+        alert_data['device_id'] = str(_dry['tank_no'])
         alert_data['device_name'] = "Tank"
         alert_data['severity'] = 'Critical' if status == 0 else 'High' if status == 1 else 'Medium' if status == 2 else 'Low'
         alert_data['indent_no'] = ''
@@ -72,11 +83,14 @@ async def indentdryout_create_dry_out_alert(data: Indentdryout_Create_Dry_Out_Al
         Charts_Connection_Vault_RoutingParams.connection_id = "1"
         Charts_Connection_Vault_RoutingParams.action = 'upsert_data'
         function = await charts_connection_vault_routing(Charts_Connection_Vault_RoutingParams)
-        return await function(
-            schema_name="HPCL_HOS",
-            table_name=connection_mapping.table_mapping.get("dry_out", ""),
-            records=_dry,
-            conflict_columns=["site_id", "fcc_code", "product_no", "tank_no"]
-        )
+        for each_tank in str(alert_data['device_id']).split(','):
+            _dry['tank_no'] = each_tank
+            await function(
+                schema_name="HPCL_HOS",
+                table_name=connection_mapping.table_mapping.get("dry_out", ""),
+                records=_dry,
+                conflict_columns=["site_id", "fcc_code", "product_no", "tank_no"]
+            )
+        return
 
     return {"status": True, "message": "Alerts created successfully", "data": []}
