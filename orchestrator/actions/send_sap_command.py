f86cbb9168466c81597c22f5128e0c2d437abd2e
YesurajuParusu468 <yesu.p@algofusiontech.com>
2024-12-11 10:50:19 +0530

    updated  send_sap_command code

--- orchestrator/actions/send_sap_command.py
+++ orchestrator/actions/send_sap_command.py
@@ -55,7 +55,7 @@ class SendSapCommand:
 
         if flag == 'U':
             await asyncio.sleep(4)
-            query = (f"vehicle_number='{vehicle_number}' and violation_type='{alert_data['violation_type']}'"
+            query = (f"vehicle_number='{vehicle_number}' and device_type='{alert_data['device_type']}'"
                      f"and alert_status ='{'Open'}'")
             ndata = await hpcl_ceg_model.Alerts.get_all(urdhva_base.queryparams.QueryParams(q=query), resp_type='plain')
             if len(ndata['data']):
@@ -82,7 +82,7 @@ class SendSapCommand:
         reason = alert_data.get('msg', 'IRIS')
         blocks = []
 
-        query = (f"vehicle_number='{vehicle_number}' and violation_type='{alert_data['violation_type']}'"
+        query = (f"vehicle_number='{vehicle_number}' and device_type='{alert_data['device_type']}'"
                      f"and alert_status ='{'Open'}'")
         aldata2 = await hpcl_ceg_model.Alerts.get_all(urdhva_base.queryparams.QueryParams(q=query), resp_type='plain')
 
@@ -190,4 +190,4 @@ class SendSapCommand:
 
         if exceptionOcr:
             return True, {"sapcommandsent": False}
-        return True, {"sapcommandsent": True} 
+        return True, {"sapcommandsent": True}
