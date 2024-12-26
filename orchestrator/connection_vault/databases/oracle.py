14ee51736d4ab471e3a6116a41ba30687da3f545
amplifinn-admin <santosh@urdhvapay.com>
2024-12-27 02:05:50 +0530

    Update oracle.py

--- orchestrator/connection_vault/databases/oracle.py
+++ orchestrator/connection_vault/databases/oracle.py
@@ -37,22 +37,22 @@ class Oracle(BaseAction):
 
     async def get_connection(self):
         if 'connection_name' in self.params.keys():
-            redis_ins = urdhva_base.redispool.get_synchronous_redis_connection()
-            redis_key = f"cred_store_{self.params['connection_name']}"
-            try:
-                if redis_ins.exists(f"cred_store_{self.params['connection_name']}"):
-                    self.params = json.loads(base64.b64decode(redis_ins.get(redis_key)))
-                else:
-                    self.params = await hpcl_ceg_model.CredsModel.get(self.params['connection_name'])
-                    redis_ins.setex(redis_key, 24 * 60 * 60,
-                                          base64.b64encode(json.dumps(self.params, default=str).encode()).decode())
-            except:
-                self.params = await hpcl_ceg_model.CredsModel.get(self.params['connection_name'])
-            finally:
-                try:
-                    redis_ins.close()
-                except:
-                    ...
+            # redis_ins = urdhva_base.redispool.get_synchronous_redis_connection()
+            # redis_key = f"cred_store_{self.params['connection_name']}"
+            # try:
+            #     if redis_ins.exists(f"cred_store_{self.params['connection_name']}"):
+            #         self.params = json.loads(base64.b64decode(redis_ins.get(redis_key)))
+            #     else:
+            #         self.params = await hpcl_ceg_model.CredsModel.get(self.params['connection_name'])
+            #         redis_ins.setex(redis_key, 24 * 60 * 60,
+            #                               base64.b64encode(json.dumps(self.params, default=str).encode()).decode())
+            # except:
+            self.params = await hpcl_ceg_model.CredsModel.get(self.params['connection_name'])
+            # finally:
+            #     try:
+            #         redis_ins.close()
+            #     except:
+            #         ...
         if not isinstance(self.params, dict):
             self.params = self.params.__dict__
         if 'credentials' in self.params.keys():
