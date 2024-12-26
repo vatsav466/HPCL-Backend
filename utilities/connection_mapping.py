5a5b75ba04a4184f4517f54b6f0a0c4598d51f44
amplifinn-admin <santosh@urdhvapay.com>
2024-12-26 14:37:31 +0530

    added valid indent

--- utilities/connection_mapping.py
+++ utilities/connection_mapping.py
@@ -30,3 +30,14 @@ camunda_listener_mapping = {
     "camunda_dryout_09": {"host": "10.90.38.166", "port": 9088},
     "camunda_dryout_10": {"host": "10.90.38.166", "port": 9089}
 }
+
+dry_out_top_x_axis = [{"name": "Indent Not Raised", "group": "not_raised"}, {"name": "Pending Indents", "group": "pending"},
+                  {"name": "Indent On Hold", "group": "pending"}, {"name": "Truck Allocated", "group": "wip"},
+                  {"name": "Sent to SAP", "group": "wip"}, {"name": "Sales Order Placed", "group": "wip"},
+                  {"name": "R2 Swiped", "group": "wip"}, {"name": "Invoice Created", "group": "wip"},
+                  {"name": "R3 Swiped", "group": "wip"}, {"name": "VTS", "group": "wip"},
+                  {"name": "Indent Delivered", "group": "delivered"}]
+
+dry_out_bottom_x_axis = [
+        "Dealer", "SO\nRM", "SO\nCO", "SO", "SO\nRM", "SO\nRM", "PO\nRM", "PO\nRM", "PO\nRM", "PO\nRM", "SO\nRM"
+    ]
