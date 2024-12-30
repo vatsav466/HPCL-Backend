3deff68d9a66e3928623adbf6df98c8c1100743f
Keerthesre1 <139453571+Keerthesre1@users.noreply.github.com>
2024-12-30 10:37:33 +0530

    Added workflow name in the interlock mapping

--- orchestrator/alerting/alert_factory.py
+++ orchestrator/alerting/alert_factory.py
@@ -147,7 +147,7 @@ class AlertFactory:
 
                 payload["variables"]["interlock_id"] = {"value": interlock['id'], "type": "String"}
                 interlock_name = interlock_mapping.get_interlock_name(bu=bu, interlock_name=interlock_name,sop_id=sop_id)
-                workflowid =interlock_name.get("workflow_name", "")
+                workflowid =interlock_name.get("workflow_name", "interlock_name")
                 workflow_id = interlock_mapping.fmt_il_name(workflowid)
                 print("payload: ", payload)
                 if alert_data_dict.get("alert_section") not in ["VA", "VTS"]:
