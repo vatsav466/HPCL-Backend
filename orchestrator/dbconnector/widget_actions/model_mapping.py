fc2517b1a35839eacf222b87f439e202724424c1
Nirmalkumar25 <160230112+NirmalAlgo@users.noreply.github.com>
2025-01-08 19:45:34 +0530

    Updated model mapping

--- orchestrator/dbconnector/widget_actions/model_mapping.py
+++ orchestrator/dbconnector/widget_actions/model_mapping.py
@@ -2,7 +2,8 @@ modelMapping = {
     'Alerts': ['high_alert_locations', 'critical_alert_locations','sod_terminal', 'alert_categories', 
                'tas_alerts', 'non_tas_alerts','no_of_terminals', 'alert_distributions'],
     
-    'LpgSalesSummaryData': ['lpg_cdcms','lpg_cdcms_month','cdcms_order_source' ],
+    'LpgSalesSummaryData': ['lpg_cdcms', 'lpg_cdcms_month', 'cdcms_order_source', 'overall_pending_pmuy_nmpuy',
+                            'pending_1_3_days'],
 
     'M60LevelMetaData':['sales_performance'],
 
