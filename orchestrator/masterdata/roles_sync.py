d724eba31bd6a0eadea3c37e75c0bf11fc09d79e
venugopalnaidu <venu@urdvapay.com>
2025-01-08 23:37:29 +0530

    updated roles_sync

---
+++ orchestrator/masterdata/roles_sync.py
@@ -0,0 +1,59 @@
+import urdhva_base
+import asyncio
+import hpcl_ceg_model
+
+
+async def sync_user_roles():
+    role_mapping = {"Zonal SOD": {"allowed_pages": [{"menu_name": "SOD Terminal",
+                                                     "allowed_sub_menus": ["Supply Chain Home", "Terminal Home",
+                                                                           "Dashboard", "Video Analytics"]},
+                                                    {"menu_name": "Supply Chain",
+                                                     "allowed_sub_menus": ["Supply Chain Home"]}], "name": "SOD",
+                                  "status": True},
+                    "Zonal Head LEVEL SOD": {"allowed_pages": [{"menu_name": "SOD Terminal",
+                                                                "allowed_sub_menus": ["Supply Chain Home",
+                                                                                      "Terminal Home", "Dashboard",
+                                                                                      "Video Analytics"]},
+                                                               {"menu_name": "Supply Chain",
+                                                                "allowed_sub_menus": ["Supply Chain Home"]}],
+                                             "name": "SOD", "status": True},
+                    "SOD Planning Officer": {"allowed_pages": [{"menu_name": "SOD Terminal",
+                                                                "allowed_sub_menus": ["Supply Chain Home",
+                                                                                      "Terminal Home", "Dashboard"]},
+                                                               {"menu_name": "Supply Chain",
+                                                                "allowed_sub_menus": ["Supply Chain Home"]}],
+                                             "name": "SOD", "status": True},
+                    "Zonal LPG SOD": {"allowed_pages": [{"menu_name": "LPG",
+                                                         "allowed_sub_menus": ["Plant", "Sales CDCMS",
+                                                                               "LPG Analytics"]}], "name": "LPG",
+                                      "status": True},
+                    "HQO SOD": {"allowed_pages": [{"menu_name": "SOD Terminal",
+                                                   "allowed_sub_menus": ["Supply Chain Home", "Terminal Home",
+                                                                         "Dashboard", "Video Analytics"]},
+                                                  {"menu_name": "Supply Chain",
+                                                   "allowed_sub_menus": ["Supply Chain Home"]}], "name": "SOD",
+                                "status": True},
+                    "Plant Incharge SOD ": {"allowed_pages": [{"menu_name": "SOD Terminal",
+                                                               "allowed_sub_menus": ["Supply Chain Home",
+                                                                                     "Terminal Home", "Dashboard",
+                                                                                     "Video Analytics"]},
+                                                              {"menu_name": "Supply Chain",
+                                                               "allowed_sub_menus": ["Supply Chain Home"]}],
+                                            "name": "SOD", "status": True},
+                    "COLA HQO Officer SOD": {
+                        "allowed_pages": [{"menu_name": "SOD Terminal", "allowed_sub_menus": ["Supply Chain Home"]}],
+                        "name": "SOD", "status": True},
+                    "HQO Head SOD": {"allowed_pages": [{"menu_name": "SOD Terminal",
+                                                        "allowed_sub_menus": ["Supply Chain Home", "Terminal Home",
+                                                                              "Dashboard", "Video Analytics"]},
+                                                       {"menu_name": "Supply Chain",
+                                                        "allowed_sub_menus": ["Supply Chain Home"]}], "name": "SOD",
+                                     "status": True},
+                    "Admin": {"allowed_pages": [], "status": True}
+                    }
+    await hpcl_ceg_model.Roles.bulk_update([{"name": key, "status": True, "allowed_pages": value["allowed_pages"]}
+                                            for key, value in role_mapping.items()])
+
+
+if __name__ == "__main__":
+    asyncio.run(sync_user_roles())
