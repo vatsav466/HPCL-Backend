c49fad0094dd35c4c2314977ad4fdc8b823bf9ca
Keerthesre1 <139453571+Keerthesre1@users.noreply.github.com>
2025-01-10 12:27:57 +0530

    Created model for geo coordinates

--- api_manager/hpcl_ceg_stdapi.py
+++ api_manager/hpcl_ceg_stdapi.py
@@ -445,3 +445,14 @@ async def get_all(response: fastapi.Response, params=fastapi.Depends(urdhva_base
 @router.delete('/momlevelfinalmetadata/{id}', tags=['MomLevelFinalMetaData'])
 async def delete(id: str):
     return await MomLevelFinalMetaData.delete(id)
+
+
+@router.get('/bulevelgeocoordinates/{id}', response_model=BuLevelGeoCoordinates, tags=['BuLevelGeoCoordinates'])
+async def get(id: str):
+    return await BuLevelGeoCoordinates.get(id, skip_secrets=True)
+
+
+@router.get('/bulevelgeocoordinates', response_model=BuLevelGeoCoordinatesGetResp, tags=['BuLevelGeoCoordinates'])
+async def get_all(response: fastapi.Response, params=fastapi.Depends(urdhva_base.queryparams.QueryParams)):
+    return await BuLevelGeoCoordinates.get_all(params, skip_secrets=True)
+
