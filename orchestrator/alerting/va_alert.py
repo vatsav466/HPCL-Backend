c0bd6726282a4b3899b34bfe3bf3bbb472269598
YesurajuParusu468 <yesu.p@algofusiontech.com>
2025-01-18 11:09:07 +0530

    added comments for va_alert

--- orchestrator/alerting/va_alert.py
+++ orchestrator/alerting/va_alert.py
@@ -38,7 +38,8 @@ class VAAlertManager(alert_factory.AlertFactory):
         try:
             logger.info(f"alert_data received to create alert {alert_data}")
 
-            #getting location_id in this form from payload example "location_id": "ACC, Bandra, 11073010, 11073010",
+            # here record['alert_section'] will be alert_type of VA
+            # Getting location_id in this form from payload example "location_id": "ACC, Bandra, 11073010, 11073010",
             location_id = alert_data['location_id'].split(",")[-1].strip()
 
             # Retrieve necessary fields from the alert_data
@@ -72,23 +73,39 @@ class VAAlertManager(alert_factory.AlertFactory):
                         "action_type": "Created",
                         "alert_status": "Open"
                         }]
+                    
+                    # here record['alert_section'] will be alert_type of VA
+                    # Retrieving the alert_mapping details for alert_type based on location_type and alert_type.
                     va_alert_data = va_alert_mapping.VA_Alert_Mapping[record['location_type']].get(
                         record['alert_section'], {})
                     if not va_alert_data:
                         logger.info("interlock_details not found")
                         continue
 
+                    # Generate a unique key using sap_id, location_type, interlock_name of alert_type and device_id 
+                    # for alert identification using multiple parameters
                     keys = [location_id, record['location_type'], "VA", record['device_id'],
                             va_alert_data['name']]
+                    
+                    # Generate a unique alert_id using the keys
                     alert_id = helpers.generate_hash(keys)
                     redis_ins = await urdhva_base.redispool.get_redis_connection()
+
+                    # Checking if the alert_id for the given alert_type already exists in Redis.
+                    # This is because we store the alert_id in Redis with a 3-hour expiration time.
+                    # If the same alert_type is received from the same device_id within the expiration period,
+                    # it will be considered a duplicate, and processing will be skipped.
                     if await redis_ins.exists(alert_id):
                         logger.info("Alert already exists")
                         continue
+
+                    # Retrieving Interlock_mapping details like sop_id, interlock_name for the alert_type 
+                    # based on location_type and interlock_name.
                     interlock_details = utilities.interlock_mapping.get_interlock_name(
                         record['location_type'],
                         va_alert_data["name"])
                     
+                    # preparing alert_data for VA
                     interlock_details.update({"bu": record['location_type'],
                                               "location_name": loc_dt['name'],
                                               "sap_id": location_id,
