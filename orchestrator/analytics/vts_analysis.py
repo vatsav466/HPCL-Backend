31deea72a22bd30e5069b23b115302a406bbcf9e
amplifinn-admin <santosh@urdhvapay.com>
2025-01-21 20:15:01 +0530

    bug fix for vts

--- orchestrator/analytics/vts_analysis.py
+++ orchestrator/analytics/vts_analysis.py
@@ -11,11 +11,11 @@ async def get_creds(db_name: str):
 
 async def is_vts_enabled(truck_no: str) -> bool:
     creds = await get_creds("VTS")
-    url = f"https://{creds['host']}:{creds['port']}/api/TTDetails/VTSEnabled"
+    url = f"http://{creds['host']}:{creds['port']}/api/TTDetails/VTSEnabled"
     session = requests.Session()
     session.auth = (creds['user'], creds['password'])
     try:
-        response = session.post(url, json={"TT_No": truck_no}, headers=default_headers)
+        response = session.post(url, params={"TT_No": truck_no}, headers=default_headers)
         if response.status_code // 100 == 2:
             return response.json()
         return response.json()
@@ -24,11 +24,11 @@ async def is_vts_enabled(truck_no: str) -> bool:
 
 async def get_tt_current_location(truck_no: str) -> typing.List[typing.Any]:
     creds = credential_loader.get_credentials("VTS")
-    url = f"https://{creds['host']}:{creds['port']}/api/TTDetails/VTSCurrentLocation"
+    url = f"http://{creds['host']}:{creds['port']}/api/TTDetails/VTSCurrentLocation"
     session = requests.Session()
     session.auth = (creds['user'], creds['password'])
     try:
-        response = session.post(url, json={"TT_No": truck_no}, headers=default_headers)
+        response = session.post(url, params={"TT_No": truck_no}, headers=default_headers)
         if response.status_code // 100 == 2:
             return response.json()
         return response.json()
@@ -37,11 +37,11 @@ async def get_tt_current_location(truck_no: str) -> typing.List[typing.Any]:
 
 async def get_trucks_available_in_terminal(terminal_plant_id: str) -> typing.List[typing.Any]:
     creds = credential_loader.get_credentials("VTS")
-    url = f"https://{creds['host']}:{creds['port']}/api/TTDetails/TT_Approching_Depot"
+    url = f"http://{creds['host']}:{creds['port']}/api/TTDetails/TT_Approching_Depot"
     session = requests.Session()
     session.auth = (creds['user'], creds['password'])
     try:
-        response = session.post(url, json={"DEPOT_ERP_CODE": terminal_plant_id}, headers=default_headers)
+        response = session.post(url, params={"DEPOT_ERP_CODE": terminal_plant_id}, headers=default_headers)
         if response.status_code // 100 == 2:
             return response.json()
         return response.json()
@@ -50,11 +50,11 @@ async def get_trucks_available_in_terminal(terminal_plant_id: str) -> typing.Lis
 
 async def get_trucks_returning_to_terminal() -> typing.List[typing.Any]:
     creds = credential_loader.get_credentials("VTS")
-    url = f"https://{creds['host']}:{creds['port']}/api/TTDetails/TT_Approching_Depot"
+    url = f"http://{creds['host']}:{creds['port']}/api/TTDetails/TT_Approching_Depot"
     session = requests.Session()
     session.auth = (creds['user'], creds['password'])
     try:
-        response = session.post(url, json={}, headers=default_headers)
+        response = session.post(url, params={}, headers=default_headers)
         if response.status_code // 100 == 2:
             return response.json()
         return response.json()
