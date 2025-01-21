5becbd5842afb37dff81312e4e3469fe7b954e2c
amplifinn-admin <santosh@urdhvapay.com>
2025-01-21 12:01:17 +0530

    Update ims_data_sync.sh

--- orchestrator/alerting/listener/ims_data_sync.sh
+++ orchestrator/alerting/listener/ims_data_sync.sh
@@ -5,3 +5,4 @@ source /opt/ceg/venv/bin/activate
 unbuffer python /opt/ceg/algo/orchestrator/alerting/listener/sync_ims_indent_products.py > /var/log/ceg_sys_logs/sync_ims_indent_products.log
 unbuffer python /opt/ceg/algo/orchestrator/alerting/listener/sync_ims_indent_request.py > /var/log/ceg_sys_logs/sync_ims_indent_request.log
 unbuffer python /opt/ceg/algo/orchestrator/alerting/listener/sync_ims_truck_swipe.py > /var/log/ceg_sys_logs/sync_ims_truck_swipe.log
+unbuffer python /opt/ceg/algo/orchestrator/alerting/listener/sync_ims_dealer_details.py > /var/log/ceg_sys_logs/sync_ims_dealer_details.log
