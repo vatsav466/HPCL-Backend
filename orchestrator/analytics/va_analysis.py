f61f91309bf9354d77585cf343d9d4d132d814f3
amplifinn-admin <santosh@urdhvapay.com>
2025-01-28 13:27:23 +0530

    Updated VTS and VA apis

---
+++ orchestrator/analytics/va_analysis.py
@@ -0,0 +1,34 @@
+import urdhva_base
+import requests
+import datetime
+import orchestrator.dbconnector.credential_loader as credential_loader
+
+
+async def get_va_headers(db_name):
+    creds = credential_loader.get_va_creds(db_name=db_name)
+    creds['url'] = f"https://{creds['host']}/api/v1/Violation/capa"
+    creds['Origin'] = f"https://{creds['host']}"
+    creds['Referer'] = f"https://{creds['host']}/home/dashboard/{creds['cust_id']}"
+    return creds
+
+
+async def close_va_alerts(params: dict):
+    creds = await get_va_headers("VA_ALERT")
+    ack_datetime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    headers = {
+        "Content-Type": "application/json",
+        "CustId": creds['cust_id'],
+        "MessageId": f"ACKNOWLEDGE_ALARM{ack_datetime}",
+        "Origin": creds['Origin'],
+        "Referer": creds['Referer'],
+        "UserId": creds['user'],
+        "ApplicationId": creds['application_id'],
+        "Cookie": creds['cookie'],
+        "SessionToken": creds['session_token']
+    }
+    response = requests.post(
+        url=creds['url'], json=params, headers=headers
+    )
+    if response.status_code // 100 == 2:
+        return {"status": True, "message": "Data posted successfully", "data": response.json()}
+    return {"status": False, "message": "Data posting unsuccessfully", "data": response.json()}
--- vendor_ingestion_api/ingestion_api_model.py
+++ vendor_ingestion_api/ingestion_api_model.py
@@ -62,7 +62,7 @@ class vtsDataUpdatedCreate(pydantic.BaseModel):
     endlongitude: typing.Optional[str] = pydantic.Field("", **{})
 
 
-class vtsBlockedTruckCreate(pydantic.BaseModel):
+class vtsBlockedTruckOldCreate(pydantic.BaseModel):
     alert_id: str
     alert_type: str
     action_type: typing.Optional[str] = pydantic.Field("", **{})
@@ -79,6 +79,20 @@ class vtsBlockedTruckCreate(pydantic.BaseModel):
     approval_status: typing.Optional[str] = pydantic.Field("", **{})
     blocked_datetime: typing.Optional[str] = pydantic.Field("", **{})
     unblocked_datetime: typing.Optional[str] = pydantic.Field("", **{})
+
+
+class vtsBlockedTruckCreate(pydantic.BaseModel):
+    alert_id: typing.Optional[str] = pydantic.Field("", **{})
+    tt__no: typing.Optional[str] = pydantic.Field("", **{})
+    location__name: typing.Optional[str] = pydantic.Field("", **{})
+    transporter__name: typing.Optional[str] = pydantic.Field("", **{})
+    transporter__code: typing.Optional[str] = pydantic.Field("", **{})
+    vehicle__blocked__desc: typing.Optional[str] = pydantic.Field("", **{})
+    vehicle__blocked__start__date: typing.Optional[str] = pydantic.Field("", **{})
+    vehicle__blocked__end__date: typing.Optional[str] = pydantic.Field("", **{})
+    vehicle__blocked__instance__no: typing.Optional[str] = pydantic.Field("", **{})
+    vehicle__blocked__instance__type: typing.Optional[str] = pydantic.Field("", **{})
+    alert__type: typing.Optional[str] = pydantic.Field("", **{})
 
 
 class Vts_Ingest_DataParams(pydantic.BaseModel):
