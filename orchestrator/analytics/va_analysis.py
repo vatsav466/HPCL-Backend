18dd5085847714ca9d3943ea2adc8eef297bb9c4
amplifinn-admin <santosh@urdhvapay.com>
2025-01-30 13:22:03 +0530

    Updated api for get va scores

--- orchestrator/analytics/va_analysis.py
+++ orchestrator/analytics/va_analysis.py
@@ -6,7 +6,6 @@ import orchestrator.dbconnector.credential_loader as credential_loader
 
 async def get_va_headers(db_name):
     creds = credential_loader.get_va_creds(db_name=db_name)
-    creds['url'] = f"https://{creds['host']}/api/v1/Violation/capa"
     creds['Origin'] = f"https://{creds['host']}"
     creds['Referer'] = f"https://{creds['host']}/home/dashboard/{creds['cust_id']}"
     return creds
@@ -31,6 +30,7 @@ async def close_va_alerts(params: dict):
 
     """
     creds = await get_va_headers("VA_ALERT")
+    creds['url'] = f"https://{creds['host']}/api/v1/Violation/capa"
     ack_datetime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     headers = {
         "Content-Type": "application/json",
@@ -49,3 +49,52 @@ async def close_va_alerts(params: dict):
     if response.status_code // 100 == 2:
         return {"status": True, "message": "Data posted successfully", "data": response.json()}
     return {"status": False, "message": "Data posting unsuccessfully", "data": response.json()}
+
+async def get_ro_terminal_scores(params: dict):
+    """
+
+    Args:
+        params:
+        {
+            LocationType(string) - RO, TAS, LPG
+            StartDate(Mandatory) - 2025-01-27(yyyy-MM-dd)
+            EndDate(Nullable) - 2025-01-27(yyyy-MM-dd) (when nullable returns only one day data W.R.T start date)
+        }
+    Returns:
+        [
+          {
+            "VENDOR_ID": "Ms_ROCKROSE_AUTO_CENTRE",
+            "LOCATION_ID": "M/s Rockrose Auto Centre ,16144210",
+            "LOCATION_TYPE": "RO",
+            "OVERALL_SCORE": "0",
+            "DATE": "2025-01-27"
+          },
+          {
+            "VENDOR_ID": "Ms_ROCKROSE_AUTO_CENTRE",
+            "LOCATION_ID": "M/s Rockrose Auto Centre ,16144210",
+            "LOCATION_TYPE": "RO",
+            "OVERALL_SCORE": "0",
+            "DATE": "2025-01-28"
+          }
+        ]
+    """
+    creds = await get_va_headers("VA_ALERT")
+    creds['url'] = f"https://{creds['host']}/api/Platform/v1/HPCLVendor/Scores"
+    ack_datetime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    headers = {
+        "Content-Type": "application/json",
+        "CustId": creds['cust_id'],
+        "MessageId": f"ACKNOWLEDGE_ALARM{ack_datetime}",
+        "Origin": creds['Origin'],
+        "Referer": creds['Referer'],
+        "UserId": creds['user'],
+        "ApplicationId": creds['application_id'],
+        "Cookie": creds['cookie'],
+        "SessionToken": creds['session_token']
+    }
+    response = requests.post(
+        url=creds['url'], json=params, headers=headers
+    )
+    if response.status_code // 100 == 2:
+        return {"status": True, "message": "Data fetched successfully", "data": response.json()}
+    return {"status": False, "message": "Data fetching unsuccessfully", "data": response.json()}
