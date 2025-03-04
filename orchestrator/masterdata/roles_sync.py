b41fb227b7783cc09519fbe0b5bb90828cab382f
Keerthesre1 <139453571+Keerthesre1@users.noreply.github.com>
2025-03-04 18:56:49 +0530

    Update roles_sync.py
    SOD roles update

--- orchestrator/masterdata/roles_sync.py
+++ orchestrator/masterdata/roles_sync.py
@@ -4,32 +4,61 @@ import hpcl_ceg_model
 
 
 async def sync_user_roles():
-    role_mapping = {"Zonal SOD": {"allowed_pages": [{"menu_name": "SOD Terminal",
-                                                     "allowed_sub_menus": ["Supply Chain", "Terminal Home",
-                                                                           "Video Analytics"]}], "name": "SOD",
-                                  "status": True},
+    role_mapping = {
+
+                    "Location In-Charge SOD": {"allowed_pages": [{"menu_name": "SOD Terminal",
+                                                                        "allowed_sub_menus": ["Supply Chain", "Terminal Home",
+                                                                                                "Video Analytics"]}],
+                                                    "name": "SOD", "status": True},
+
+                    "Safety Officer SOD": {"allowed_pages": [{"menu_name": "SOD Terminal",
+                                                                        "allowed_sub_menus": ["Supply Chain", "Terminal Home",
+                                                                                                "Video Analytics"]}],
+                                                    "name": "SOD", "status": True},
+                    
+                    "M&I Officer SOD": {"allowed_pages": [{"menu_name": "SOD Terminal",
+                                                                        "allowed_sub_menus": ["Supply Chain", "Terminal Home",
+                                                                                                "Video Analytics"]}],
+                                                    "name": "SOD", "status": True},
+
+
+                    "Zonal Safety Officer SOD": {"allowed_pages": [{"menu_name": "SOD Terminal",
+                                                                "allowed_sub_menus": ["Supply Chain", "Terminal Home",
+                                                                                    "Video Analytics"]}], "name": "SOD",
+                                            "status": True},
+
+                    "Zonal M&I Officer SOD": {"allowed_pages": [{"menu_name": "SOD Terminal",
+                                                                "allowed_sub_menus": ["Supply Chain", "Terminal Home",
+                                                                                    "Video Analytics"]}], "name": "SOD",
+                                            "status": True},
+                    
+                    "Zonal SOD": {"allowed_pages": [{"menu_name": "SOD Terminal",
+                                                                "allowed_sub_menus": ["Supply Chain", "Terminal Home",
+                                                                                    "Video Analytics"]}], "name": "SOD",
+                                            "status": True},
+
                     "Zonal Head LEVEL SOD": {"allowed_pages": [{"menu_name": "SOD Terminal",
                                                                 "allowed_sub_menus": ["Supply Chain",
-                                                                                      "Terminal Home", "Dashboard",
-                                                                                      "Video Analytics"]}],
-                                             "name": "SOD", "status": True},
+                                                                                        "Terminal Home", "Dashboard",
+                                                                                        "Video Analytics"]}],
+                                                        "name": "SOD", "status": True},
+
                     "SOD Planning Officer": {"allowed_pages": [{"menu_name": "SOD Terminal",
                                                                 "allowed_sub_menus": ["Supply Chain", "Terminal Home",
                                                                                       "Video Analytics"]}],
                                              "name": "SOD", "status": True},
+
                     "HQO SOD": {"allowed_pages": [{"menu_name": "SOD Terminal",
                                                    "allowed_sub_menus": ["Supply Chain", "Terminal Home",
                                                                          "Video Analytics"]},
                                                   {"menu_name": "Supply Chain",
                                                    "allowed_sub_menus": ["Supply Chain Home"]}], "name": "SOD",
                                 "status": True},
-                    "Plant Incharge SOD": {"allowed_pages": [{"menu_name": "SOD Terminal",
-                                                              "allowed_sub_menus": ["Supply Chain", "Terminal Home",
-                                                                                    "Video Analytics"]}],
-                                           "name": "SOD", "status": True},
+                    
                     "COLA HQO Officer SOD": {
                         "allowed_pages": [{"menu_name": "SOD Terminal", "allowed_sub_menus": ["Supply Chain"]}],
                         "name": "SOD", "status": True},
+
                     "HQO Head SOD": {"allowed_pages": [{"menu_name": "SOD Terminal",
                                                         "allowed_sub_menus": ["Supply Chain", "Terminal Home" 
                                                                                               "Video Analytics"]},
