b209c16893018b117e1a66112981c9b9457737b3
amplifinn-admin <santosh@urdhvapay.com>
2025-03-19 01:31:22 +0530

    Updated dealer tt details

--- api_manager/indentdryout_actions.py
+++ api_manager/indentdryout_actions.py
@@ -650,6 +650,8 @@ async def indentdryout_get_dried_out_ro(data: Indentdryout_Get_Dried_Out_RoParam
     more_than_15_days = dry_out_aging.get("more_than_15_days", 0)
     indent_not_raised_count = less_than_2_days + from_3_to_7_days + from_8_to_15_days + more_than_15_days
     count_50_klm = await dry_out_analysis.get_ro_count_less_50(conditions)
+    tar_analysis = await dry_out_analysis.get_tar_analysis(conditions)
+    dealer_tt_count = await dry_out_analysis.get_tt_counts(conditions)
 
     stats = [{"section": top_x_axis[key - 1]['name'], "value": value, "serial": key, "condition": "=",
               "group": top_x_axis[key - 1]['group']}
@@ -699,22 +701,22 @@ async def indentdryout_get_dried_out_ro(data: Indentdryout_Get_Dried_Out_RoParam
             "section": "Indent Not Raised", "value": indent_not_raised_count, "serial": 29,
             "condition": "=", "group": "tar_analysis"
         }, {
-            "section": "< 1 Rs Cr", "value": 0, "serial": 30,
+            "section": "< 1 Rs Cr", "value": tar_analysis.get("less_1_cr", 0), "serial": 30,
             "condition": "=", "group": "tar_analysis"
         }, {
-            "section": "1 to 2 Rs Cr", "value": 0, "serial": 31,
+            "section": "1 to 2 Rs Cr", "value": tar_analysis.get("less_2_cr", 0), "serial": 31,
             "condition": "=", "group": "tar_analysis"
         }, {
-            "section": "2 to 5 Rs Cr", "value": 0, "serial": 32,
+            "section": "2 to 5 Rs Cr", "value": tar_analysis.get("less_5_cr", 0), "serial": 32,
             "condition": "=", "group": "tar_analysis"
         }, {
-            "section": "> 5 Rs Cr", "value": 0, "serial": 33,
+            "section": "> 5 Rs Cr", "value": tar_analysis.get("greater_5_cr", 0), "serial": 33,
             "condition": "=", "group": "tar_analysis"
         },{
-            "section": "Dealer TT", "value": 0, "serial": 34,
+            "section": "Dealer TT", "value": dealer_tt_count, "serial": 34,
             "condition": "=", "group": "tt_pending"
         }, {
-            "section": "Transport TT", "value": 0, "serial": 35,
+            "section": "Transport TT", "value": indent_not_raised_count - dealer_tt_count, "serial": 35,
             "condition": "=", "group": "tt_pending"
         }, {
             "section": "Dealer TT Return", "value": 0, "serial": 36,
