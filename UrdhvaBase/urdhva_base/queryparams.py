aada2da2336da95e181e19f9cbf08d91e318da9d
Keerthesre1 <139453571+Keerthesre1@users.noreply.github.com>
2025-04-08 10:03:17 +0530

    Update queryparams.py
    Added % to regex pattern for search text

--- UrdhvaBase/urdhva_base/queryparams.py
+++ UrdhvaBase/urdhva_base/queryparams.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel, Field
 
 class QueryParams(BaseModel):
     q: Optional[str] = Query(None, description="Filter query to be executed against the database.")
-    search_text: Optional[str] = Query(None, description="Content to search on", regex='^([a-zA-Z0-9_.\\-=" ]+|)$')
+    search_text: Optional[str] = Query(None, description="Content to search on", regex='^([a-zA-Z0-9_.\\-%=" ]+|)$')
     skip: int = Field(0, ge=0, le=10000, description="Number of records to skip")
     limit: int = Field(100, ge=0, le=10000, description="Max number of records to fetch")
     sort: Optional[str] = None
