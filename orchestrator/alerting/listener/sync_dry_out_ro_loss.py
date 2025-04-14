95dd650c0ba4bd0f66cfe5863afafe55ba76abbb
amplifinn-admin <santosh@urdhvapay.com>
2025-04-14 19:56:47 +0530

    removed limit for ro loss

--- orchestrator/alerting/listener/sync_dry_out_ro_loss.py
+++ orchestrator/alerting/listener/sync_dry_out_ro_loss.py
@@ -69,7 +69,7 @@ async def sync_dry_out_ro_loss():
                  AND ap.sales_product_no::bigint = a.product_no::bigint
                  AND ap.tank_no::bigint = a.tank_no::bigint
                 ORDER BY ap.start_ts DESC"""
-    data = await hpcl_ceg_model.Alerts.get_aggr_data(query=query, limit=1000)
+    data = await hpcl_ceg_model.Alerts.get_aggr_data(query=query, limit=0)
     data = pd.DataFrame(data.get("data", []))
     products_map = {'2811000': 'MS', '1322000': 'MS', '2812000': 'HSD', '1683000': 'HSD', '3912000': 'TURBO',
                     '1683100': 'TURBO', '2816000': 'POWER 99', '2682000': 'POWER 99', '3672000': 'POWER 95',
