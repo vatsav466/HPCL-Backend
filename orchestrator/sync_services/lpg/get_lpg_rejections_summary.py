9c9e688aa7bb21c438a3f74a0d097000bc787fb7
ShriHariB7 <160230113+ShriHariB7@users.noreply.github.com>
2025-04-17 16:05:49 +0530

    lpg operations code update
    lpg operations code update

--- orchestrator/sync_services/lpg/get_lpg_rejections_summary.py
+++ orchestrator/sync_services/lpg/get_lpg_rejections_summary.py
@@ -205,6 +205,9 @@ def get_cs_rejections(params):
     query = """ SELECT MAX(process_date) from production_log; """
     max_date = fetch_data(query, getData=False, params=params)
     
+    if not last_date:
+        last_date = (datetime.now() - relativedelta(days=5)).strftime("%Y-%m-%d")
+        
     query = f""" WITH base_data AS (
                     SELECT
                         system_id,
@@ -294,6 +297,8 @@ def get_gd_rejections(params):
     last_date = fetch_data(query, getData=False, params=params, internal=True)
     query = """ SELECT MAX(process_date) from event_log; """
     max_date = fetch_data(query, getData=False, params=params)
+    if not last_date:
+        last_date = (datetime.now() - relativedelta(days=5)).strftime("%Y-%m-%d")
     
     query = f""" WITH base_stats AS (
                     SELECT
@@ -363,6 +368,8 @@ def get_pt_rejections(params):
     #    return    
     query = f""" SELECT MAX(max_date) from lpg_pt_rejections WHERE "plant"='{params['PlantName']}'; """
     last_date = fetch_data(query, getData=False, params=params, internal=True)
+    if not last_date:
+        last_date = (datetime.now() - relativedelta(days=5)).strftime("%Y-%m-%d")
     query = """ SELECT MAX(process_date) from event_log; """
     max_date = fetch_data(query, getData=False, params=params)
     
