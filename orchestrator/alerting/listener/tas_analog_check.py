7341f299bd8ed3abe37bc6f46d8793fd7cbec151
Manohar477 <manohar.v@algofusiontech.com>
2025-04-26 16:17:49 +0530

    tas_analog check file updated

--- orchestrator/alerting/listener/tas_analog_check.py
+++ orchestrator/alerting/listener/tas_analog_check.py
@@ -7,7 +7,7 @@ import hpcl_ceg_model
 import uuid
 from datetime import datetime, timedelta
 import tas_duplicate_alert_check as duplicates_check
-from orchestrator.alerting.alert_manager import create_alert 
+import orchestrator.alerting.alert_factory as alert_factory
 
 
 async def tas_bcu_analog():
@@ -26,41 +26,55 @@ async def tas_bcu_analog():
         for interlock_name in interlock_names:
             # Query to get the count of alerts for each device_name for the interlock in the current week
             query1 = f"""
-                SELECT device_name, COUNT(*) as alert_count
+                SELECT device_name, sap_id, COUNT(*) as alert_count
                 FROM alerts
                 WHERE interlock_name = '{interlock_name}'
                 AND created_at BETWEEN '{start_of_week_str}' AND '{end_of_week_str}'
-                GROUP BY device_name
+                GROUP BY device_name, sap_id
             """
-            params = urdhva_base.queryparams.QueryParams(q=query1)
-            resp1 = await hpcl_ceg_model.Alerts.get_all(params, resp_type='plain')
+           
+            try:
+                resp1 = await hpcl_ceg_model.Alerts.get_aggr_data(query1)
+            except Exception as e:
+                print(f"Error executing query: {e}")
+                continue
+            # Check if resp1 contains data
+            if not resp1.get("data", []):
+                print(f"No data found for interlock: {interlock_name}")
+                continue
             
             # Process the response
             for record in resp1.get("data", []):
                 device_name = record.get("device_name")
+                if not device_name:
+                    print(f"Device name is missing in the record: {record}")
+                    continue
+                sap_id = record.get("sap_id", "")
                 alert_count = record.get("alert_count", 0)
                 
                 # Trigger alert only when exactly 3 occurrences are found in the week
-                if alert_count == 3:
+                if alert_count > 2:
                    # Create alert data
                     query2 = f"""
-                        SELECT severity,  sap_id , device_type
+                        SELECT severity, device_type, bu,
                         FROM alerts
                         WHERE interlock_name = '{interlock_name}'
                         AND device_name = '{device_name}'
                         ORDER BY created_at DESC
-                        LIMIT 1
                     """
-                    params = urdhva_base.queryparams.QueryParams(q=query2)
-                    resp2 = await hpcl_ceg_model.Alerts.get_all(params, resp_type='plain')
+                    
+                    resp2 = await hpcl_ceg_model.Alerts.get_aggr_data(query2)
                     if resp2.get("data", []):
                        severity = resp2["data"][0].get("severity", "")
-                       sap_id = resp2["data"][0].get("sap_id", "")
+                       bu = resp2["data"][0].get("bu", "")
+                       device_type = resp2["data"][0].get("device_type", "")
                     alert_data = {
                         "interlock_name": "K Factor BCU Primissive Off_Fail",
                         "device_name": device_name,
                         "sop_id": "SOP28A",
                         "sap_id": sap_id,
+                        "bu": bu,
+                        "device_type": device_type,
                         "severity": severity,
                         "alert_id": str(uuid.uuid1())     
                     }
@@ -77,10 +91,13 @@ async def tas_bcu_analog():
                         print(f"Alert history exists for {interlock_name} on device {device_name}. Skipping alert creation.")
                         continue
                     # Create the alert
-                    success, msg = await create_alert(alert_data)
+                    success, msg = await alert_factory.AlertFactory.create_alert(alert_data)
                     if success:
                         print(f"Weekly summary alert created successfully for {interlock_name} on device {device_name}.")
                     else:
                         print(f"Failed to create weekly summary alert for {interlock_name} on device {device_name}: {msg}")
     except Exception as e:
         print(f"Error in tas_bcu_analog: {traceback.format_exc()}")
+
+if __name__ == "__main__":
+    asyncio.run(tas_bcu_analog())
