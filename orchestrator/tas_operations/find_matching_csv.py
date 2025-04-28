74d3bd8a85374169ec63457cf0431e174383371d
ShriHariB7 <160230113+ShriHariB7@users.noreply.github.com>
2025-04-28 12:32:28 +0530

    send write command code update
    send write command code update

--- orchestrator/tas_operations/find_matching_csv.py
+++ orchestrator/tas_operations/find_matching_csv.py
@@ -3,14 +3,14 @@ import aiofiles
 import pandas as pd
 
 
-async def find_matching_row(csv_file_path, match_criteria):
+async def find_matching_row(csv_file_path, match_criteria, analog=False):
     """
     Finds the first row in a csv file that matches the given criteria.
 
     Args:
         csv_file_path (str): The path to the CSV file.
         match_criteria (dict): A dictionary containing the criteria to match against the CSV rows.
-    
+    `
     Returns:
          dict: The first matching row as a dictionary, or None if no match is found.    
     """ 
@@ -24,6 +24,9 @@ async def find_matching_row(csv_file_path, match_criteria):
             # Check if the row matches the criteria
             if all(str(row.get(key,'')).strip().lower() == str(value).strip().lower() for key, value in match_criteria.items()):
                 print(f"found matching row : {row}")
+                return row
+            elif match_criteria.get("equipment_name").strip().lower() in row.get("equipment_name", "").strip().lower():
+                print(f"found matching substring row : {row}")
                 return row
         return None
     except Exception as e:
