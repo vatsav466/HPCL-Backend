db6bb6be8e57b3b0a80eee49f755945a7b78e34a
Manohar477 <manohar.v@algofusiontech.com>
2025-05-13 10:43:11 +0530

    Updated action code for tas_update_proof_test.py

--- orchestrator/actions/tas_update_proof_test.py
+++ orchestrator/actions/tas_update_proof_test.py
@@ -12,7 +12,7 @@ class TasUpdateProofTest:
     async def get_required_variables(self):
         return ["alert_id"]
     
-    async def check_proof_test(self, params):
+    async def handle_updateProofTest(self, params):
         """
         Check and update the proof test data in the tas_proof_test table.
         If the data already exists, update the proof_test_created_at and next_proof_test_date.
@@ -27,11 +27,12 @@ class TasUpdateProofTest:
             # Extracting required parameters from alert data
             interlock_name = alert_data.get('interlock_name')
             equipment_name = alert_data.get('tas_device_name') or alert_data.get('device_name')
+            location_name = alert_data.get('location_name')
             device_id = alert_data.get('device_id')
             created_at = alert_data.get('created_at')
             sap_id = alert_data.get('sap_id')
 
-            if not (interlock_name and equipment_name and sap_id and created_at):
+            if not (interlock_name and equipment_name and sap_id and created_at and device_id and location_name): 
                 print("Missing required fields")
 
             # Convert created_at to a datetime object
@@ -48,6 +49,7 @@ class TasUpdateProofTest:
                 query = f"""
                     SELECT * FROM tas_proof_test
                     WHERE device_name = '{equipment_name}' AND sap_id = '{sap_id}' AND device_id = '{device_id}' AND interlock_name = '{interlock_name}'
+                    AND location_name = '{location_name}'
                 """
                 existing_record = await hpcl_ceg_model.TasProofTest.get_aggr_data(query)
 
@@ -56,15 +58,6 @@ class TasUpdateProofTest:
                 existing_record["next_proof_test_date"] = next_proof_test_date.strftime("%Y-%m-%d %H:%M:%S")
                 data_obj = hpcl_ceg_model.TasProofTest(**existing_record)
                 await data_obj.modify()
-                # Update the existing record
-                # update_query = f"""
-                #     UPDATE tas_proof_test
-                #     SET proof_test_created_at = '{created_at.strftime("%Y-%m-%d %H:%M:%S")}',
-                #         next_proof_test_date = '{next_proof_test_date.strftime("%Y-%m-%d %H:%M:%S")}'
-                #     WHERE device_name = '{equipment_name}' AND sap_id = '{sap_id} AND device_id = '{device_id} AND interlock_name = '{interlock_name}'
-                # """
-                # call update query query execute function for tas_proof_test
-                # await hpcl_ceg_model.TasProofTest.update_by_query(update_query)
                 print(f"Updated proof test record for device: {equipment_name}, SAP ID: {sap_id}")
                 return True , {"message": "Moved to next block"}
             else:
@@ -72,19 +65,12 @@ class TasUpdateProofTest:
                     "interlock_name": interlock_name,
                     "equipment_name": equipment_name,
                     "device_id": device_id,
+                    "location_name": location_name,
                     "sap_id": sap_id,
                     "proof_test_created_at": created_at.strftime("%Y-%m-%d %H:%M:%S"),
                     "next_proof_test_date": next_proof_test_date
                 }
                 await hpcl_ceg_model.TasProofTestCreate(**data).create()
-
-                # # Insert a new record
-                # insert_query = f"""
-                #     INSERT INTO tas_proof_test (device_name, device_id, sap_id, interlock_name, proof_test_created_at, next_proof_test_date)
-                #     VALUES ('{equipment_name}', {device_id}, '{sap_id}', {interlock_name}, '{created_at.strftime("%Y-%m-%d %H:%M:%S")}', '{next_proof_test_date.strftime("%Y-%m-%d %H:%M:%S")}')
-                # """
-                # # call insert query  execute function for tas_proof_test
-                # print(f"Inserted new proof test record for device: {equipment_name}, SAP ID: {sap_id}")
                 return True, { "message": "Moved to next block"}
 
         except Exception as e:
