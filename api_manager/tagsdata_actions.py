fc83fa48f8c008bc19d38f2aa2e87f7687563249
Keerthesre1 <139453571+Keerthesre1@users.noreply.github.com>
2025-05-21 19:38:24 +0530

    mfm query corrections

--- api_manager/tagsdata_actions.py
+++ api_manager/tagsdata_actions.py
@@ -33,7 +33,7 @@ async def tagsdata_things_board_device_data(data: Tagsdata_Things_Board_Device_D
         if location_df.empty:
             return {"status": False, "message": "No TAS locations found."}
         
-        mfm_query = "SELECT sap_id, COUNT(bcu_number) as mfm_count FROM host_mfm_factor WHERE bcu_number IS NOT NULL GROUP BY sap_id"
+        mfm_query = "SELECT sap_id, COUNT(DISTINCT mfm_number) AS mfm_count FROM host_mfm_factor where bcu_number is not NULL GROUP BY sap_id ORDER BY sap_id;"
         mfm_df = await execute_query(query=mfm_query)
         mfm_df = pd.DataFrame(mfm_df)
         mfm_map = dict(zip(mfm_df['sap_id'],mfm_df['mfm_count']))
@@ -217,7 +217,7 @@ async def tagsdata_things_board_device_data(data: Tagsdata_Things_Board_Device_D
                     "name": location_name,
                     "zone": zone,
                     "device_type": "MFM",
-                    "count": str(mfm_map[sap_id]),
+                    "count": str(mfm_df['mfm_count']),
                     "system": "Gantry"  # MFM is always classified as Gantry
                 }
                 
