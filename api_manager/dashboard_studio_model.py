79111e9ce706af16e3ef376d55e393ee8d7e9b5d
vgc <venu@urdhvapay.com>
2025-06-17 10:15:31 +0530

    Updated regex conditions

--- api_manager/dashboard_studio_model.py
+++ api_manager/dashboard_studio_model.py
@@ -122,8 +122,8 @@ class filtered_keysCreate(pydantic.BaseModel):
 class WidgetFiltersCreate(pydantic.BaseModel):
     key: str = pydantic.Field(**{'pattern': '^[a-zA-Z0-9_.\\-=" ]+$'})
     cond: str
-    value: typing.Optional[str] = pydantic.Field("", **{'pattern': '^([a-zA-Z0-9,\\[\\]\\{\\}\\(\\)&><#_.\\-=" ]+|)$'})
-    val: typing.Optional[str] = pydantic.Field("", **{'pattern': '^([a-zA-Z0-9,\\[\\]\\{\\}\\(\\)&><#_.\\-=" ]+|)$'})
+    value: typing.Optional[str] = pydantic.Field("", **{'pattern': '^[a-zA-Z0-9,+\[\]\{\}\(\)&><#_.\-=" ]*$'})
+    val: typing.Optional[str] = pydantic.Field("", **{'pattern': '^[a-zA-Z0-9,+\[\]\{\}\(\)&><#_.\-=" ]*$'})
 
 
 class ChartsSchema(UrdhvaPostgresBase):
--- orchestrator/reporting_services/novex_daily_report.py
+++ orchestrator/reporting_services/novex_daily_report.py
@@ -463,7 +463,7 @@ async def send_notification(notification_data):
     await ins.publish_message(
         subject="Novex Daily Report",
         recipients=["sanjayk@hpcl.in", "cvmallinath@hpcl.in", "debeshp@hpcl.in",
-                    "purushm@hpcl.in", "sachinkwarghane@hpcl.in", "dinesh.kumar@hpcl.in"],
+                    "purushm@hpcl.in", "sachinkwarghane@hpcl.in", "dinesh.kumar@hpcl.in", "gargam@hpcl.in", "rujutadoiphode@hpcl.in"],
         html_content=True,
         body=final_data,
         force_send=True
