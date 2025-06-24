dcffb8e266fd69a6fae84d57bea795dd6977d97c
amplifinn-admin <santosh@urdhvapay.com>
2025-06-24 12:59:11 +0530

    Update ims_data_sync.sh

--- orchestrator/alerting/listener/ims_data_sync.sh
+++ orchestrator/alerting/listener/ims_data_sync.sh
@@ -2,8 +2,8 @@
 
 cd /opt/ceg/algo/api_manager
 source /opt/ceg/venv/bin/activate
+unbuffer python /opt/ceg/algo/orchestrator/alerting/listener/sync_ims_truck_swipe.py > /var/log/ceg_sys_logs/sync_ims_truck_swipe.log
 unbuffer python /opt/ceg/algo/orchestrator/alerting/listener/sync_ims_indent_products.py > /var/log/ceg_sys_logs/sync_ims_indent_products.log
 unbuffer python /opt/ceg/algo/orchestrator/alerting/listener/sync_ims_indent_request.py > /var/log/ceg_sys_logs/sync_ims_indent_request.log
-unbuffer python /opt/ceg/algo/orchestrator/alerting/listener/sync_ims_truck_swipe.py > /var/log/ceg_sys_logs/sync_ims_truck_swipe.log
 unbuffer python /opt/ceg/algo/orchestrator/alerting/listener/sync_ims_dealer_details.py > /var/log/ceg_sys_logs/sync_ims_dealer_details.log
 unbuffer python /opt/ceg/algo/orchestrator/alerting/listener/sync_auto_dc_requests.py > /var/log/ceg_sys_logs/sync_auto_dc_requests.log
