5fa586bb928a34f2c99b0490c5efd24ebc268ae4
ShriHariB7 <shrihari.b@algofusiontech.com>
2025-08-04 11:00:29 +0530

    Roles Code Updated
    Roles Code Updated

--- orchestrator/masterdata/roles_sync.py
+++ orchestrator/masterdata/roles_sync.py
@@ -1296,6 +1296,25 @@ async def sync_user_roles():
         "name": "Sales",
         "status": True
     },
+    "Sales Officer I&C": {
+        "allowed_pages": [
+            {
+                "menu_name": "Performance",
+                "allowed_sub_menus": [
+                    {
+                        "title": "Sales Performance",
+                        "allowed_sub_menus": [
+                            {
+                            "title": "I&C Insights"
+                            }
+          ]
+                    }
+                ]
+            }
+        ],
+        "name": "Sales",
+        "status": True
+    },
     "Admin": {
         "allowed_pages": [],
         "status": True
