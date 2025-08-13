f87e965e028a662bd43fd250d128885b86cdd8e6
kushal <kushalshettymn@gmail.com>
2025-08-13 13:21:54 +0530

    infra_functions code updated

--- orchestrator/dashboard/chart_factory/infra_functions.py
+++ orchestrator/dashboard/chart_factory/infra_functions.py
@@ -351,7 +351,7 @@ async def get_download_info(sbu, background_tasks):
             return {"status": "error", "message": "No data found."}
 
         df = pd.DataFrame(rows)
-        output_dir = "/Users/apple/Documents/Infra_output"  # temporary folder
+        output_dir = "/opt/ceg/algo/orchestrator/masterdata/infra_inputs"  # temporary folder
         os.makedirs(output_dir, exist_ok=True)
         template_file_path = os.path.join(output_dir, f"Updated_{sbu}_infra_data.xlsx")
         df.to_excel(template_file_path, index=False, sheet_name=sbu)
