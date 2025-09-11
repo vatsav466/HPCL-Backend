70abeef5c9ef6b654cb2d2b818cd49722e5fd633
YesurajuParusu468 <yesu.p@algofusiontech.com>
2025-09-11 13:42:18 +0530

    updated instance mapping for VTS

--- utilities/vts_instance_mapping.py
+++ utilities/vts_instance_mapping.py
@@ -1,3 +1,18 @@
+''' 
+Here
+0 = Instance 1
+1 = Instance 2
+2 = Instance 3
+
+Ex: 
+For instance one 
+>> Speed violation 
+instance_1 count = 3 which is used to add logic like > 3
+instance_2 count = 2 which is used to add logic like > 2
+Here for instance_2 
+check the count like >3 which is on 4th count and adding >2 which is 3 then total will be 7
+'''
+
 instance_mapping = {
     "TAS":{
         "0":{
