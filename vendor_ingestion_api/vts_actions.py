a00905f38f084b5e1b1447fc9ef1b573487fe006
Manohar477 <manohar.v@algofusiontech.com>
2025-10-07 17:47:22 +0530

    update the ongoing trip details apis

--- vendor_ingestion_api/vts_actions.py
+++ vendor_ingestion_api/vts_actions.py
@@ -181,3 +181,94 @@ async def vts_ingest_trip_data(data: Vts_Ingest_Trip_DataParams):
         print(traceback.format_exc())
         logger.error(e)
         return {"status": False, "message": "Error", "data": []}
+
+
+# Action trip_not_closed
+@router.post('/trip_not_closed', tags=['VTS'])
+async def vts_trip_not_closed(data: Vts_Trip_Not_ClosedParams):
+    try:        
+        # Ensure data.data is a list and contains items
+        if isinstance(data.data, list) and len(data.data) > 0:
+            enriched_data = jsonable_encoder(data.data)
+        else:
+            logger.error(f"Invalid data structure: data.data is not a list or is empty")
+            return {"status": False, "message": "Invalid data", "data": []}
+        logger.info(f"Received VTS data ingestion from vendor {enriched_data}")
+        print(f"Received VTS data ingestion from vendor {enriched_data}")
+
+        # redis_queue = urdhva_base.redispool.RedisQueue('vts_alerts_queue')
+        # await redis_queue.put(json.dumps(enriched_data))
+        return True, "Success"
+
+    except Exception as e:
+        print(traceback.format_exc())
+        logger.error(e)
+        return {"status": False, "message": "Error", "data": []}
+
+
+# Action trip_without_route
+@router.post('/trip_without_route', tags=['VTS'])
+async def vts_trip_without_route(data: Vts_Trip_Without_RouteParams):
+    try:        
+        # Ensure data.data is a list and contains items
+        if isinstance(data.data, list) and len(data.data) > 0:
+            enriched_data = jsonable_encoder(data.data)
+        else:
+            logger.error(f"Invalid data structure: data.data is not a list or is empty")
+            return {"status": False, "message": "Invalid data", "data": []}
+        logger.info(f"Received VTS data ingestion from vendor {enriched_data}")
+        print(f"Received VTS data ingestion from vendor {enriched_data}")
+
+        # redis_queue = urdhva_base.redispool.RedisQueue('vts_alerts_queue')
+        # await redis_queue.put(json.dumps(enriched_data))
+        return True, "Success"
+
+    except Exception as e:
+        print(traceback.format_exc())
+        logger.error(e)
+        return {"status": False, "message": "Error", "data": []}
+
+
+# Action route_deviation_more_than_two_km
+@router.post('/route_deviation_more_than_two_km', tags=['VTS'])
+async def vts_route_deviation_more_than_two_km(data: Vts_Route_Deviation_More_Than_Two_KmParams):
+    try:        
+        # Ensure data.data is a list and contains items
+        if isinstance(data.data, list) and len(data.data) > 0:
+            enriched_data = jsonable_encoder(data.data)
+        else:
+            logger.error(f"Invalid data structure: data.data is not a list or is empty")
+            return {"status": False, "message": "Invalid data", "data": []}
+        logger.info(f"Received VTS data ingestion from vendor {enriched_data}")
+        print(f"Received VTS data ingestion from vendor {enriched_data}")
+
+        # redis_queue = urdhva_base.redispool.RedisQueue('vts_alerts_queue')
+        # await redis_queue.put(json.dumps(enriched_data))
+        return True, "Success"
+
+    except Exception as e:
+        print(traceback.format_exc())
+        logger.error(e)
+        return {"status": False, "message": "Error", "data": []}
+
+# Action trip_unauthorised_stoppage
+@router.post('/trip_unauthorised_stoppage', tags=['VTS'])
+async def vts_trip_unauthorised_stoppage(data: Vts_Trip_Unauthorised_StoppageParams):
+    try:        
+        # Ensure data.data is a list and contains items
+        if isinstance(data.data, list) and len(data.data) > 0:
+            enriched_data = jsonable_encoder(data.data)
+        else:
+            logger.error(f"Invalid data structure: data.data is not a list or is empty")
+            return {"status": False, "message": "Invalid data", "data": []}
+        logger.info(f"Received VTS data ingestion from vendor {enriched_data}")
+        print(f"Received VTS data ingestion from vendor {enriched_data}")
+
+        # redis_queue = urdhva_base.redispool.RedisQueue('vts_alerts_queue')
+        # await redis_queue.put(json.dumps(enriched_data))
+        return True, "Success"
+
+    except Exception as e:
+        print(traceback.format_exc())
+        logger.error(e)
+        return {"status": False, "message": "Error", "data": []}
