c7965480df860e97d2a81f8112702fb809be19b3
ShriHariB7 <shrihari.b@algofusiontech.com>
2025-10-15 16:47:02 +0530

    LPG Plant Operations Code Updated
    LPG Plant Operations Code Updated

--- orchestrator/sync_services/lpg/lpg_operations_data_sync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /opt/ceg/algo/orchestrator/sync_services/lpg
-source /opt/ceg/venv/bin/activate
-unbuffer python /opt/ceg/algo/orchestrator/sync_services/lpg/lpg_get_rejections.py > /var/log/ceg_sys_logs/lpg/lpg_rejection_data_sync_$(date +\%Y-\%m-\%d_\%H-\%M).log 2>&1
-unbuffer python /opt/ceg/algo/orchestrator/sync_services/lpg/fetch_lpg_operations_data.py > /var/log/ceg_sys_logs/lpg/lpg_operations_data_sync_$(date +\%Y-\%m-\%d_\%H-\%M).log 2>&1
