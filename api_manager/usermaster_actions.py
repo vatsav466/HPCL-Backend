251217ff4885dadbe6899ad193a04b60e0980b22
YesurajuParusu468 <yesu.p@algofusiontech.com>
2025-12-17 16:31:03 +0530

    bug fix usermaster creation

--- api_manager/usermaster_actions.py
+++ api_manager/usermaster_actions.py
@@ -20,7 +20,7 @@ async def usermaster_create_user(data: Usermaster_Create_UserParams):
         if rpt and rpt.get('username','') in ['admin','superadmin']:
             return False, "Not allowed to perform this operation"
         
-        if data.username in ['admin','superadmin']:
+        if data.data.username in ['admin','superadmin']:
             return False, "Not allowed to perform this operation"
 
         if not isinstance(data.data,dict):
