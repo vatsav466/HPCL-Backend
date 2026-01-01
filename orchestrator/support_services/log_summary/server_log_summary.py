d9237675a8896071a996110bcb5933ea40af5ee9
YesurajuParusu468 <yesu.p@algofusiontech.com>
2026-01-01 17:06:49 +0530

    removed urdhvapay mail

--- orchestrator/support_services/log_summary/server_log_summary.py
+++ orchestrator/support_services/log_summary/server_log_summary.py
@@ -325,7 +325,6 @@ async def send_mail(excel_path):
             "bala@algofusiontech.com",
             "venu@algofusiontech.com",
             "moufikali@algofusiontech.com",
-            "vamsi.c@urdhvapay.com",
             "yesu.p@algofusiontech.com",
             "manohar.v@algofusiontech.com",
             "mohith.p@algofusiontech.com",
