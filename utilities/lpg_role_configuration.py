d1f03803bf00f59832627c331f9a0d5f7756ff6a
poojithagumma-svg <poojitha.gumma@algofusiontech.com>
2026-01-02 19:17:19 +0530

    update lpg_role_configuration

--- utilities/lpg_role_configuration.py
+++ utilities/lpg_role_configuration.py
@@ -8,29 +8,29 @@ lpg_role_mapping={
                 "level - 1": {
                     "condition": "<",
                     "value": 0,
-                    "assign_role": "Maintenance Officer LPG",
-                    "escalation_role": "Maintenance Officer LPG",
+                    "assign_role": "Safety Officer LPG,Location In-Charge LPG,Maintenance Officer LPG,Planning Officer LPG",
+                    "escalation_role": "Safety Officer LPG,Location In-Charge LPG,Maintenance Officer LPG,Planning Officer LPG",
                     "escalation_time": "PT24H"
                 },
                 "level - 2": {
                     "condition": "<>",
                     "value": 1,
-                    "assign_role": "Location In-Charge LPG",
-                    "escalation_role": "Location In-Charge LPG",
+                    "assign_role": "Zonal HSE LPG,Zonal Operations Chief Manager LPG,Zonal Head LPG,Zonal Officer LPG,Zonal Operations LPG",
+                    "escalation_role": "Zonal HSE LPG,Zonal Operations Chief Manager LPG,Zonal Head LPG,Zonal Officer LPG,Zonal Operations LPG",
                     "escalation_time": "PT24H"
                 },
                 "level - 3": {
                     "condition": ">",
                     "value": 2,
-                    "assign_role": "Zonal Operations Head LPG",
-                    "escalation_role": "Zonal Operations Head LPG",
+                    "assign_role": "HQO LPG,HQO Head LPG,HQO Sale General Manager,HQ Operations LPG,HQO HSE LPG",
+                    "escalation_role": "HQO LPG,HQO Head LPG,HQO Sale General Manager,HQ Operations LPG,HQO HSE LPG",
                     "escalation_time": "PT24H"
                 },
                 "level - 4": {
                     "condition": ">",
                     "value": 3,
-                    "assign_role": "HQO LPG",
-                    "escalation_role": "HQO LPG",
+                    "assign_role": "HQO LPG,HQO Head LPG,HQO Sale General Manager,HQ Operations LPG,HQO HSE LPG",
+                    "escalation_role": "HQO LPG,HQO Head LPG,HQO Sale General Manager,HQ Operations LPG,HQO HSE LPG",
                     "escalation_time": "PT24H"
                 }
             }
@@ -43,29 +43,29 @@ lpg_role_mapping={
                 "level - 1": {
                     "condition": "<",
                     "value": 0,
-                    "assign_role": "Maintenance Officer LPG",
-                    "escalation_role": "Maintenance Officer LPG",
+                    "assign_role": "Safety Officer LPG,Location In-Charge LPG,Maintenance Officer LPG,Planning Officer LPG",
+                    "escalation_role": "Safety Officer LPG,Location In-Charge LPG,Maintenance Officer LPG,Planning Officer LPG",
                     "escalation_time": "PT24H"
                 },
                 "level - 2": {
                     "condition": "<>",
                     "value": 1,
-                    "assign_role": "Location In-Charge LPG",
-                    "escalation_role": "Location In-Charge LPG",
+                    "assign_role": "Zonal HSE LPG,Zonal Operations Chief Manager LPG,Zonal Head LPG,Zonal Officer LPG,Zonal Operations LPG",
+                    "escalation_role": "Zonal HSE LPG,Zonal Operations Chief Manager LPG,Zonal Head LPG,Zonal Officer LPG,Zonal Operations LPG",
                     "escalation_time": "PT24H"
                 },
                 "level - 3": {
                     "condition": ">",
                     "value": 2,
-                    "assign_role": "Zonal Operations Head LPG",
-                    "escalation_role": "Zonal Operations Head LPG",
+                    "assign_role": "HQO LPG,HQO Head LPG,HQO Sale General Manager,HQ Operations LPG,HQO HSE LPG",
+                    "escalation_role": "HQO LPG,HQO Head LPG,HQO Sale General Manager,HQ Operations LPG,HQO HSE LPG",
                     "escalation_time": "PT24H"
                 },
                 "level - 4": {
                     "condition": ">",
                     "value": 3,
-                    "assign_role": "HQO LPG",
-                    "escalation_role": "HQO LPG",
+                    "assign_role": "HQO LPG,HQO Head LPG,HQO Sale General Manager,HQ Operations LPG,HQO HSE LPG",
+                    "escalation_role": "HQO LPG,HQO Head LPG,HQO Sale General Manager,HQ Operations LPG,HQO HSE LPG",
                     "escalation_time": "PT24H"
                 }
             }
@@ -78,29 +78,29 @@ lpg_role_mapping={
                 "level - 1": {
                     "condition": "<",
                     "value": 0,
-                    "assign_role": "Maintenance Officer LPG",
-                    "escalation_role": "Maintenance Officer LPG",
+                    "assign_role": "Safety Officer LPG,Location In-Charge LPG,Maintenance Officer LPG,Planning Officer LPG",
+                    "escalation_role": "Safety Officer LPG,Location In-Charge LPG,Maintenance Officer LPG,Planning Officer LPG",
                     "escalation_time": "PT24H"
                 },
                 "level - 2": {
                     "condition": "<>",
                     "value": 1,
-                    "assign_role": "Location In-Charge LPG",
-                    "escalation_role": "Location In-Charge LPG",
+                    "assign_role": "Zonal HSE LPG,Zonal Operations Chief Manager LPG,Zonal Head LPG,Zonal Officer LPG,Zonal Operations LPG",
+                    "escalation_role": "Zonal HSE LPG,Zonal Operations Chief Manager LPG,Zonal Head LPG,Zonal Officer LPG,Zonal Operations LPG",
                     "escalation_time": "PT24H"
                 },
                 "level - 3": {
                     "condition": ">",
                     "value": 2,
-                    "assign_role": "Zonal Operations Head LPG",
-                    "escalation_role": "Zonal Operations Head LPG",
+                    "assign_role": "HQO LPG,HQO Head LPG,HQO Sale General Manager,HQ Operations LPG,HQO HSE LPG",
+                    "escalation_role": "HQO LPG,HQO Head LPG,HQO Sale General Manager,HQ Operations LPG,HQO HSE LPG",
                     "escalation_time": "PT24H"
                 },
                 "level - 4": {
                     "condition": ">",
                     "value": 3,
-                    "assign_role": "HQO LPG",
-                    "escalation_role": "HQO LPG",
+                    "assign_role": "HQO LPG,HQO Head LPG,HQO Sale General Manager,HQ Operations LPG,HQO HSE LPG",
+                    "escalation_role": "HQO LPG,HQO Head LPG,HQO Sale General Manager,HQ Operations LPG,HQO HSE LPG",
                     "escalation_time": "PT24H"
                 }
             }
