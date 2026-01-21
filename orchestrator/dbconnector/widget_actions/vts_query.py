afc46a222b38602172a4f7ee1379c221a73b41f5
Manohar477 <manohar.v@algofusiontech.com>
2026-01-21 16:20:46 +0530

    Update vts_query.py

--- orchestrator/dbconnector/widget_actions/vts_query.py
+++ orchestrator/dbconnector/widget_actions/vts_query.py
@@ -171,7 +171,7 @@ vts_query = {
 
     "blocked_alerts_download" : """
                                    SELECT * FROM alerts  WHERE  alert_section = 'VTS'
-                                   AND vehicle_unblocked_date is null and
+                                   AND vehicle_unblocked_date is null 
                                    AND interlock_name NOT IN ('Itdg Admin Blocked','No VTS No Load') AND
                                    device_id IN ('Instance - 1', 'Instance - 2', 'Instance - 3')	                                                                                          
                                 """,
@@ -179,7 +179,7 @@ vts_query = {
     "auto_unblock_download" : """
                               SELECT * FROM alerts WHERE  alert_section = 'VTS'
                               AND alert_status = 'Close' and mark_as_false = false and
-                              vehicle_unblocked_date is not null  AND 
+                              vehicle_unblocked_date is not null 
                               AND interlock_name NOT IN ('Itdg Admin Blocked','No VTS No Load') AND
                               device_id IN ('Instance - 1', 'Instance - 2', 'Instance - 3')                                                     
                               """,
@@ -188,7 +188,7 @@ vts_query = {
                                 SELECT * FROM alerts
                                 WHERE  alert_section = 'VTS'
                                 AND alert_status = 'Close' AND mark_as_false = true and
-                                vehicle_unblocked_date is not null AND
+                                vehicle_unblocked_date is not null 
                                 AND interlock_name NOT IN ('Itdg Admin Blocked','No VTS No Load') AND
                                 device_id IN ('Instance - 1', 'Instance - 2', 'Instance - 3')
                                 """,                          
