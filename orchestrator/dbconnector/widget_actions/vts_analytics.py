f9c20fd63d672d1f6ee4b74a8e09aee2a1bc013d
chandra <chandrasekhar.u@algofusiontech.com>
2026-02-05 11:28:20 +0530

    Update vts_analytics.py

--- orchestrator/dbconnector/widget_actions/vts_analytics.py
+++ orchestrator/dbconnector/widget_actions/vts_analytics.py
@@ -882,13 +882,7 @@ class VTSAnalyticsActions:
         if "completed_trips_risk_score" in query.lower():
             return f"scheduled_trip_start_datetime BETWEEN '{start}' AND '{end}'"
         
-        if "cluster_master" in query.lower():
-            return f"version_date BETWEEN '{start}' AND '{end}'"
-        
-        if "transporter_risk_score" in query.lower():
-            return f"version_date BETWEEN '{start}' AND '{end}'"
-        
-        if "tt_risk_score" in query.lower():
+        if any(term in query.lower() for term in ["cluster_master", "transporter_risk_score", "tt_risk_score"]):
             return f"version_date BETWEEN '{start}' AND '{end}'"
                 
         queries = ["vts_device_removed", "vts_harsh_acceleration", "vts_harsh_braking", "vts_panic"]
