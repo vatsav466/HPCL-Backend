da129f7d1f4f359611f24bcf32e36db329ddb323
Gayathri <gayathri.m@algofusiontech.com>
2026-03-26 10:54:12 +0530

    removed rename logic below in sales
    removed rename logic below in sales

--- orchestrator/sync_services/sales/sales_daywise.py
+++ orchestrator/sync_services/sales/sales_daywise.py
@@ -140,7 +140,9 @@ def insertToDB(data, table_name, indexing_col=()):
     if "PLANT_CD" in data.columns:
         data = data.rename({"PLANT_CD": "plant_cd"})
 
-     
+    if 'plant_cd' not in data.columns:
+        data = data.with_columns(pl.lit("").cast(pl.Utf8).alias("plant_cd"))
+        
     dtype_dict = {'String': str('text'), 'Int64': str('bigint'), 'Int32': str('bigint'), 'Boolean': str('text'),
                   'Float64': str('double precision'), 'Float32': str('double precision'),
                   #'Float64':'Float64',
@@ -191,15 +193,9 @@ def insertToDB(data, table_name, indexing_col=()):
     columns = []
     for i in column_names:
         columns.append(i)
-    # print("data.columns--------------->",data.columns)
-    # print("dataaaaaaaaaaaaaaa",data['plant_cd'].unique())
     
     data = data.select(columns)
-    if "PLANT_CD" in data.columns:
-        data = data.rename({"PLANT_CD": "plant_cd"})
-    if 'plant_cd' not in data.columns:
-    
-        data = data.with_columns(pl.lit("").cast(pl.Utf8).alias("plant_cd"))
+
     #data.write_csv('/tmp/sales_data.csv')
     print(data)
     #data.write_csv(f"/tmp/table_name.csv",separator='~')
