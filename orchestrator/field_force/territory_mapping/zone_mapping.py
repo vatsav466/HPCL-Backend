dc1a1086017e54a1e20600f7340d775185a23230
YesurajuParusu468 <yesu.p@algofusiontech.com>
2026-03-27 19:50:25 +0530

    added loss_of_sales_volume
    added loss_of_sales_volume

--- orchestrator/field_force/territory_mapping/zone_mapping.py
+++ orchestrator/field_force/territory_mapping/zone_mapping.py
@@ -1,3 +1,50 @@
 zone_mapping = {
-
+    "CEN": {
+        "IMS": "",
+        "CRIS": "CENTRAL ZONE"
+    },
+    "ECZ": {
+        "IMS": "",
+        "CRIS": "EAST CENTRAL ZONE"
+    },
+    "EZ": {
+        "IMS": "",
+        "CRIS": "EAST"
+    },
+    "NCZ": {
+        "IMS": "",
+        "CRIS": "NORTH CENTRAL RETAIL"
+    },
+    "NFZ": {
+        "IMS": "",
+        "CRIS": "NORTH FRONTIER ZONE"
+    },
+    "NWF": {
+        "IMS": "",
+        "CRIS": "NORTH WEST FRONTIER"
+    },
+    "NWZ": {
+        "IMS": "",
+        "CRIS": "NORTH WEST RETAIL ZO"
+    },
+    "NZ": {
+        "IMS": "",
+        "CRIS": "NORTH"
+    },
+    "SCZ": {
+        "IMS": "",
+        "CRIS": "SOUTH CENTRAL RETAIL"
+    },
+    "SWZ": {
+        "IMS": "",
+        "CRIS": "SOUTH WESTERN ZONE"
+    },
+    "SZ": {
+        "IMS": "",
+        "CRIS": "SOUTH"
+    },
+    "WZ": {
+        "IMS": "",
+        "CRIS": "WEST"
+    }
 }
