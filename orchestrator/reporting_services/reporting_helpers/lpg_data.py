700081c7192c18b32d2988d2eb7cdcba3c070434
Poojitha Gumma <poojitha.gumma@algofusiontech.com>
2026-06-02 12:14:00 +0530

    Update lpg_data.py

--- orchestrator/reporting_services/reporting_helpers/lpg_data.py
+++ orchestrator/reporting_services/reporting_helpers/lpg_data.py
@@ -1001,7 +1001,8 @@ async def log_count_excel():
         missing_event_col = 16
         event_max_date = [5, 11]
         prod_max_date = [8, 14]
-        today = datetime.datetime.now().date()
+        now = datetime.datetime.now(zoneinfo.ZoneInfo("Asia/Kolkata")).replace(tzinfo=None)
+        today = (now - datetime.timedelta(hours=2)).replace(minute=0, second=0, microsecond=0)
 
         for col_idx, value in enumerate(row):
             if isinstance(value, tuple):
@@ -1028,7 +1029,7 @@ async def log_count_excel():
                 fmt = dup_format 
             elif col_idx in event_max_date or col_idx in prod_max_date:
                 if isinstance(raw_value, datetime.datetime):
-                    if raw_value.date() < today:
+                    if raw_value.date() <= today:
                         fmt = fail_format
 
             worksheet.write(row_idx, col_idx, value, fmt)
